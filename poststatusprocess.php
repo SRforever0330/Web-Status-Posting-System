a98abd85caea07040af56af73e2ba6a3e38addad
ZHUOYA ZHOU <71320489+SRforever0330@users.noreply.github.com>
2022-10-06 18:18:54 +1100

    Add files via upload

---
+++ poststatusprocess.php
@@ -0,0 +1,21 @@
+<?php
+    $statuscode=$_POST['statuscode'];
+    $status=$_POST['status'];
+    $share=$_POST['share'];
+    $date=$_POST['date'];
+    $permission=$_POST['permission'];
+?>
+    alert($statuscode,$status,$share,$date,$permission);
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>search status form</title>
+</head>
+<body>
+	<div class="content">
+	<?php ?>
+	</div>
+</body>
+</html>
